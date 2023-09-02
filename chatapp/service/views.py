20291f889b1d900d1fc241758d771e5adfef49e0
Aslan Zurabov <76749033+AslanZ-5@users.noreply.github.com>
2023-09-02 20:19:57 +0300

    d

--- chatapp/service/views.py
+++ chatapp/service/views.py
@@ -1,5 +1,7 @@
 # from django.shortcuts import render
+from django.db.models import Count
 from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
 from .models import Service
@@ -12,11 +14,31 @@ class ServiceListViewSet(viewsets.ViewSet):
     def list(self, request):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
+        by_user = request.query_params.get("by_user") == "true"
+        by_serviceid = request.query_params.get("by_serviceid")
+        with_num_members = request.query_params.get("with_num_members")
 
+        if by_user or by_serviceid and not request.user.is_authenticated:
+            raise AuthenticationFailed()
         if category:
             self.queryset = self.queryset.filter(category=category)
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)
         if qty:
             self.queryset = self.queryset[: int(qty)]
-
-        serializer = ServiceSerializer(self.queryset, many=True)
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
+        if by_serviceid:
+            try:
+                self.queryset = self.queryset.filter(id=by_serviceid)
+                if not self.queryset.exists():
+                    raise ValidationError(
+                        detail=f"Service with id {by_serviceid} not found"
+                    )
+            except ValueError:
+                raise ValidationError(detail="Service value error")
+        serializer = ServiceSerializer(
+            self.queryset, many=True, context={"num_members": with_num_members}
+        )
         return Response(serializer.data)
